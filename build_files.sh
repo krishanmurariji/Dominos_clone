e14344bc6bd4eb0e2af58c3141a2ae6efef5768e
Krishan <Krishanmurari.gjimt@gmail.com>
2024-03-29 00:39:35 +0530

    up5

--- build_files.sh
+++ build_files.sh
@@ -1,11 +1,14 @@
-# Install Python 3.9 and pip
-# Use the appropriate package manager for the Vercel build environment
-apk add --no-cache python3.9 python3.9-dev py3-pip
+# Install pip for system Python
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3.9 get-pip.py
 
 # Install Django and other dependencies
 python3.9 -m pip install django djangorestframework
 
 # Collect static files
 python3.9 manage.py collectstatic --no-input
+
+# Remove temporary file
+rm get-pip.py
 
 echo "BUILD END"
